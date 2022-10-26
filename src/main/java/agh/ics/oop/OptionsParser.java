626872a2f06058370a4b6762b6d56fc4f5b2726b
Mpieta <maps321.mp@gmail.com>
2022-10-26 17:46:26 +0200

    lab3

--- src/main/java/agh/ics/oop/OptionsParser.java
+++ src/main/java/agh/ics/oop/OptionsParser.java
@@ -10,14 +10,10 @@ public class OptionsParser {
         MoveDirection[] dirArr = new MoveDirection[str.length];
         for(String s: str){
             switch(s) {
-                case "f" -> dirArr[i] = MoveDirection.FORWARD;
-                case "r" -> dirArr[i] = MoveDirection.RIGHT;
-                case "l" -> dirArr[i] = MoveDirection.LEFT;
-                case "b" -> dirArr[i] = MoveDirection.BACKWARD;
-                case "forward" -> dirArr[i] = MoveDirection.FORWARD;
-                case "backward" -> dirArr[i] = MoveDirection.BACKWARD;
-                case "left" -> dirArr[i] = MoveDirection.LEFT;
-                case "right" -> dirArr[i] = MoveDirection.RIGHT;
+                case "f", "forward" -> dirArr[i] = MoveDirection.FORWARD;
+                case "r", "right" -> dirArr[i] = MoveDirection.RIGHT;
+                case "l", "left" -> dirArr[i] = MoveDirection.LEFT;
+                case "b", "backward" -> dirArr[i] = MoveDirection.BACKWARD;
                 default -> {
                     dirArr[i] = MoveDirection.UNKNOWN;
                     unknown_cnt++;
