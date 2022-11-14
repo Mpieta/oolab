fc75264f2ea6ba982455d8f3167a6f4673cf9d9c
Mpieta <maps321.mp@gmail.com>
2022-11-14 12:19:55 +0100

    lab5 1.1

--- src/main/java/agh/ics/oop/RectangularMap.java
+++ src/main/java/agh/ics/oop/RectangularMap.java
@@ -3,15 +3,15 @@ package agh.ics.oop;
 import java.util.ArrayList;
 
 public class RectangularMap implements IWorldMap {
-    private int width;
-    private int height;
+    private Vector2d lower;
+    private Vector2d upper;
     public ArrayList<Animal> animalList;
     private MapVisualizer visualiser;
 
 
     public RectangularMap(int width, int height) {
-        this.width = width;
-        this.height = height;
+        this.lower = new Vector2d(0,0);
+        this.upper = new Vector2d(width-1, height-1);
 
         this.animalList = new ArrayList<Animal>();
         this.visualiser = new MapVisualizer(this);
@@ -20,12 +20,12 @@ public class RectangularMap implements IWorldMap {
 
     @Override
     public String toString() {
-        return visualiser.draw(new Vector2d(0,0), new Vector2d(width-1, height-1));
+        return visualiser.draw(lower, upper);
     }
 
     @Override
     public boolean canMoveTo(Vector2d position) {
-        return position.precedes(new Vector2d(this.width-1, this.height-1)) && position.follows(new Vector2d(0, 0)) && !this.isOccupied(position);
+        return position.precedes(upper) && position.follows(lower) && !this.isOccupied(position);
     }
 
     @Override
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,12 +4,12 @@ package agh.ics.oop;
 public class World {
 
     public static void main(String[] args) throws InterruptedException {
-        /*MoveDirection[] directions = new OptionsParser().parse(args);
+        MoveDirection[] directions = new OptionsParser().parse(args);
         IWorldMap map = new RectangularMap(10, 5);
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        //System.out.println(map);*/
+        //System.out.println(map);
 
         GrassField map1 = new GrassField(50);
         map1.place(new Animal(map1, new Vector2d(0,0)));
